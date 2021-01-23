a46048855ae33bec84a85c45f1a56aa5eb6932d4
horia.dragomir <dragomirhoria@yahoo.com>
2021-01-23 19:55:51 +0200

    alabala

--- app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/ContulMeuFragment.java
+++ app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/ContulMeuFragment.java
@@ -4,19 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import ro.pub.cs.systems.eim.myapplicationf.R;
 
 public class ContulMeuFragment  extends Fragment {
 
+    Button paginaPrincipala;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        paginaPrincipala = view.findViewById(R.id.exit);
+
+        paginaPrincipala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = (FragmentManager) getActivity().getSupportFragmentManager();
+                fm.popBackStack ("contulmeu", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            }
+        });
     }
 
     @Nullable
