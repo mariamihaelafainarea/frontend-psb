a4e8f0f041ec0e88c3d109ca151f1ad5942b30f7
horia.dragomir <dragomirhoria@yahoo.com>
2021-01-23 21:11:29 +0200

    refactored following fragments: Pagina Principala, Index, Facturi si Plati, Contul Meu

--- app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/IndexFragment.java
+++ app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/IndexFragment.java
@@ -1,6 +1,5 @@
 package ro.pub.cs.systems.eim.myapplicationf.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,23 +12,30 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import ro.pub.cs.systems.eim.myapplicationf.LoginActivity;
 import ro.pub.cs.systems.eim.myapplicationf.R;
 
 public class IndexFragment extends Fragment {
 
     ImageView buttonTransmitereIndex;
+    ImageView buttonIstoricIndex;
     Button paginaPrincipala;
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         buttonTransmitereIndex = view.findViewById(R.id.cerc_transmitereindex);
-        paginaPrincipala = view.findViewById(R.id.exit);
+        buttonIstoricIndex = view.findViewById(R.id.cerc_istoricindex);
+        paginaPrincipala = view.findViewById(R.id.exit_index);
 
         buttonTransmitereIndex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonIstoricIndex.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -43,14 +49,6 @@ public class IndexFragment extends Fragment {
                 fm.popBackStack ("index", FragmentManager.POP_BACK_STACK_INCLUSIVE);
             }
         });
-
-
-
-
-
-
-
-
     }
 
     @Nullable
