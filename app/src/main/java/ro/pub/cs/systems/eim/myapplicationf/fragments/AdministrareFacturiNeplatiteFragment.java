63b434e6e608f20c3cc47d39d58f8fb5dbf338ed
mariamihaelafainarea <mariamihaelafainarea4@gmail.com>
2021-01-27 19:39:45 +0200

    Merge branch 'master' of https://github.com/mariamihaelafainarea/frontend-psb
    # Conflicts:
    #	app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/ContulMeuFragment.java

--- app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/AdministrareFacturiNeplatiteFragment.java
+++ app/src/main/java/ro/pub/cs/systems/eim/myapplicationf/fragments/AdministrareFacturiNeplatiteFragment.java
@@ -18,27 +18,11 @@ import ro.pub.cs.systems.eim.myapplicationf.R;
 
 public class AdministrareFacturiNeplatiteFragment extends Fragment {
 
-    Spinner lunaSpinner;
     Button inapoiButton;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        lunaSpinner = view.findViewById(R.id.spinner_luna_administrarefacturineplatite);
-        inapoiButton = view.findViewById(R.id.exit_admnistrarefacturineplatite);
-
-        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
-                .createFromResource(getActivity(), R.array.lunidinan,
-                        android.R.layout.simple_spinner_item);
-        lunaSpinner.setPrompt("Selecteaza luna!");
-        staticAdapter
-                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        lunaSpinner.setAdapter(staticAdapter);
-        String ianuarie = "Ianuarie";
-        int spinnerPosition = staticAdapter.getPosition(ianuarie);
-        lunaSpinner.setSelection(spinnerPosition);
-        lunaSpinner.setBackgroundColor(Color.parseColor("#a9a9a9"));
 
         inapoiButton.setOnClickListener(new View.OnClickListener() {
             @Override
